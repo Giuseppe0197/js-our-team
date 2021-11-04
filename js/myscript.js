f9c6bed515ea2aa51f68e1ae885c6c16a8d0e34f
Giuseppe0197 <faro.g@hotmail.it>
2021-11-04 14:15:40 +0100

    descrizione passaggi js

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+/* Ricreiamo le card del nostro team aggiungendo al layout di base fornito, il nostro javascrip */
+
+
+/* creaimo le variabili nesessarie per stampare in pagina le varie immagini con i relativi nomi e ruoli */
+
+/* creiamo un'array che contiene gli oggetti necessari per creare le varie sezioni */
+
+/* creiamo gli elementi nella pagina */
+
+/* permettiamo all'utente di inserire una propria immagine mediante il form già fornito */
