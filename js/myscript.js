e8d93a2feaa945797a1c70cbef3dcf0b2f8c9d7b
Giuseppe0197 <faro.g@hotmail.it>
2021-11-04 16:47:21 +0100

    stampa prime immagini e tentativo nuovo form

--- js/myscript.js
+++ js/myscript.js
@@ -3,7 +3,7 @@
 
 /* creaimo le variabili nesessarie per stampare in pagina le varie immagini con i relativi nomi e ruoli */
 
-let containerTeam = document.getElementById("team-container");
+let containerTeam = document.querySelector(".team-container");
 
 /* creiamo un'array che contiene gli oggetti necessari per creare le varie sezioni */
 
@@ -49,28 +49,54 @@ var teams = [
 
 /* creiamo una stringa vuota per inserire gli elementi */
 
-let teamElements = ""
+let teamElements = "";
 
 /* creiamo gli elementi nella pagina */
 
 for (let i = 0; i < teams.length; i++){
 
-    let objTeam = teams[i];
+    /* creaiamo le variabili per gli oggetti dell'array */
 
-    for (let key in objTeam){
-        console.log(key, objTeam[key]);
+    let teamImg = "";
+
+    let teamName = "";
+
+    let teamRole = "";
+
+    /* facciamo un ciclo for in per assegnare il valore corrispondente degll'oggetto che andrà preso per crearlo in html */
+
+    for (let key in teams){
+
+        /* console.log(key, objTeam[key]); */
+
+        teamImg = teams[i].imgteam;
+
+        teamName = teams[i].nameTeam;
+
+        teamRole = teams[i].roleTeam;
+
     }
 
     teamElements += `
-        <div class"team-card">
+
+        <div class="team-card">
+
             <div class="card-image">
-                <img src=""
+
+                <img src="${teamImg}"
+
                 />
+
             </div>
+
             <div class="card-text">
-                <h3></h3>
-                <p></p>
+
+                <h3>${teamName}</h3>
+
+                <p>${teamRole}</p>
+                
             </div>
+
         </div>
     `
 
@@ -78,6 +104,42 @@ for (let i = 0; i < teams.length; i++){
 
 /* stampiamo gli elementi sulla pagina */
 
-containerTeam.innerHTML = teamElements;
+containerTeam.innerHTML += teamElements;
 
 /* permettiamo all'utente di inserire una propria immagine mediante il form già fornito */
+
+/* creiamo una variabile per il bottone del form */
+
+let buttonAdd = document.getElementById("addMemberButton");
+
+/* creiamo una variabile che andrà a prendere i risultati inseriti dall'utente */
+
+buttonAdd.addEventListener("click",
+
+    function() {
+
+        /* creiamo le variabili per far inserire all'utente i dati richiesti */
+
+        let nameAdd = document.getElementById("name").value;
+
+        let roleAdd = document.getElementById("role").value;
+
+        let imageAdd = document.getElementById("image").value;
+
+        let newTeamSubject = {
+
+            "imgteam" : imageAdd,
+            "nameTeam" : nameAdd,
+            "roleTeam" : roleAdd
+        
+        };
+
+        /* pushamo il nuovo oggetto */
+        
+        teams.push(newTeamSubject);
+
+        console.log(teams);
+
+    }
+
+)
